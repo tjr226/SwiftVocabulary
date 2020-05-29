310f5e14918d8d0d2338c3c87381a17d3d997d30
tjr226 <tjr226@gmail.com>
2020-05-29 18:04:56 -0400

    partway through assignment

---
+++ Swift Vocabulary/Swift Vocabulary/VocabularyWord.swift
@@ -0,0 +1,15 @@
+//
+//  VocabularyWord.swift
+//  Swift Vocabulary
+//
+//  Created by Timothy Rooney on 5/29/20.
+//  Copyright © 2020 Timothy Rooney. All rights reserved.
+//
+
+import UIKit
+
+struct VocabularyWord {
+    let word: String
+    let definition: String
+}
+
